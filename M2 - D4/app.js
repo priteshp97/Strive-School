45ead7ef41d7c2ad330e110466135dab0a8ed236
priteshp97 <88158414+priteshp97@users.noreply.github.com>
2021-10-07 15:41:28 +0100

    homework unfinished

---
+++ M2 - D4/app.js
@@ -0,0 +1,46 @@
+const nameInput = document.getElementById("names");
+const btnTeam = document.getElementById("btn-team");
+const checkNamesBtn = document.getElementById("check-names");
+const list = document.querySelector(".list");
+const teamDiv = document.querySelector(".team-section");
+const assignBtn = document.getElementById("assign");
+
+let array = [];
+
+checkNamesBtn.addEventListener("click", function () {
+  let names = document.getElementById("names").value;
+  let list = document.querySelector(".list");
+  let listItems = document.createElement("li");
+  let message = `${names}`;
+  listItems.innerHTML = message;
+  list.appendChild(listItems);
+  array.push(names);
+  nameInput.value = "";
+  console.log(array);
+});
+
+btnTeam.addEventListener("click", function () {
+  const teamSection = document.querySelector(".team-section");
+  let teamsValue = document.getElementById("teams").value;
+  for (i = 1; i <= teamsValue; i++) {
+    let list1 = document.createElement("div");
+    let html = `
+    <h2> Team ${i} </h2>
+    <ol> </ol>
+    
+    `;
+    list1.innerHTML = html;
+    teamSection.appendChild(list1);
+  }
+});
+
+assignBtn.addEventListener("click", function () {
+  let amountOfNames = document.querySelectorAll(".list li");
+  let random = Math.floor(Math.random() * array.length);
+
+  array.splice(random, 1);
+  amountOfNames[0].innerText = "";
+  console.log(array);
+  console.log(random);
+  console.log(amountOfNames[random]);
+});
