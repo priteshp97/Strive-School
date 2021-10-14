18e20f4deec403ea799498bd53196a7091d547a7
priteshp97 <88158414+priteshp97@users.noreply.github.com>
2021-10-14 16:04:11 +0100

    ex 5 js

--- M2 - D9/app.js
+++ M2 - D9/app.js
@@ -2,8 +2,9 @@ const songs = document.querySelectorAll(".song");
 const remove = document.querySelectorAll(".remove");
 
 for (i = 0; i < remove.length; i++) {
-  remove.addEventListener("click", function () {
-    songs.remove();
-    console.log("hello");
+  let temp = i;
+  remove[i].addEventListener("click", function () {
+    songs[temp].remove();
+    console.log("test");
   });
 }
