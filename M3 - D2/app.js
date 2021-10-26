a7552ad14e885fee82c6d48a8b96244fe8a7c1eb
priteshp97 <88158414+priteshp97@users.noreply.github.com>
2021-10-26 16:08:58 +0100

    more promises practise

--- M3 - D2/app.js
+++ M3 - D2/app.js
@@ -2,27 +2,53 @@
 //   resp.json();
 // });
 
+const h1 = document.querySelector(".one");
+const h2 = document.querySelector(".two");
+const h3 = document.querySelector(".three");
 const btn = document.querySelector(".btn");
 const img = document.querySelector(".img-container");
 const url = "https://picsum.photos/200/300";
 
 btn.addEventListener("click", () => {
-  loadImage(url).then((data) => console.log(data));
+  addColor(1000, h1, "red")
+    .then(() => addColor(2000, h2, "blue"))
+    .then(() => addColor(3000, h3, "green"))
+    .catch((err) => console.log(err));
 });
 
-function loadImage(url) {
-  const prom = new Promise((resolve, reject) => {
-    let img = new Image();
-    img.addEventListener("load", () => {
-      resolve(img);
-    });
-    img.addEventListener("error", () => {
-      reject(new Error("failed to load image from source url"));
-    });
-    img.src = url;
+function addColor(time, element, color) {
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(new Error(`there is no such element ${element}`));
+    }
   });
-  return prom;
 }
+
+// btn.addEventListener("click", () => {
+//   loadImage(url)
+//     .then((data) => img.appendChild(data))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// function loadImage(url) {
+//   return new Promise((resolve, reject) => {
+//     let img = new Image();
+//     img.addEventListener("load", () => {
+//       resolve(img);
+//     });
+//     img.addEventListener("error", () => {
+//       reject(new Error("failed to load image from source url"));
+//     });
+//     img.src = url;
+//   });
+// }
 
 // const promise = new Promise((resolve, reject) => {
 //   let value = true;
