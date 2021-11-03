251f18e19907ca457f8173a89700d7a204ce4dce
priteshp97 <88158414+priteshp97@users.noreply.github.com>
2021-11-03 15:34:38 +0000

    populate page

--- M3 - D8/app.js
+++ M3 - D8/app.js
@@ -1,13 +1,15 @@
 const bodyData = {
-  name: "app test 1",
-  description: "somthing longer",
+  name: "Nokia 3550",
+  description: "some description",
   brand: "nokia",
   imageUrl:
-    "https://drop.ndtv.com/TECH/product_database/images/2152017124957PM_635_nokia_3310.jpeg?downsize=*:420&output-quality=80", //REQUIRED
-  price: 100,
+    "https://drop.ndtv.com/TECH/product_database/images/2152017124957PM_635_nokia_3310.jpeg?downsize=*:420&output-quality=80",
+  price: 500,
 };
 
 let json = JSON.stringify(bodyData);
+
+let array = ["6182a701aacaa2001552a59e", "6182a70faacaa2001552a59f"];
 
 const obj = {
   method: "POST",
@@ -25,12 +27,61 @@ let api = async () => {
       "https://striveschool-api.herokuapp.com/api/product/",
       obj
     );
-    let body = await response.json();
-    console.log(body);
+    if (response.ok) {
+      let body = await response.json();
+      let container = document.querySelector(".container");
+      container.innerHTML += `
+        <article class="card">
+        <div class="img-container">
+          <img
+            src="${body.imageUrl}"
+            alt=""
+          />
+        </div>
+        <div class="body">
+        <p>${body.name}</p>
+        <p>${body.description}</p>
+          <p>${body.brand}</p>
+          <p>${"£" + body.price}</p>
+        </div>
+      </article>`;
+    } else {
+      throw new Error("Something went wrong");
+    }
   } catch (err) {
     console.log(err);
   }
 };
+
+const get = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOTRjYWFhY2FhMjAwMTU1MmExOWMiLCJpYXQiOjE2MzU5NDc3MjIsImV4cCI6MTYzNzE1NzMyMn0.iADXUoghFqm1Ar9In_6vTvfSvSVRBnJUL0Qr7XOFva8",
+  },
+};
+
+array.forEach((element) => {
+  fetch(`https://striveschool-api.herokuapp.com/api/product/${element}`, get)
+    .then((response) => response.json())
+    .then((dat) => {
+      let container = document.querySelector(".container");
+      container.innerHTML += `
+        <article class="card">
+        <div class="img-container">
+          <img
+            src="${dat.imageUrl}"
+            alt=""
+          />
+        </div>
+        <div class="body">
+        <p>${dat.name}</p>
+        <p>${dat.description}</p>
+          <p>${dat.brand}</p>
+          <p>${"£" + dat.price}</p>
+        </div>
+      </article>`;
+    });
+});
 
 // fetch("https://striveschool-api.herokuapp.com/api/product/", {
 //   headers: {
@@ -43,4 +94,4 @@ let api = async () => {
 //     console.log(body);
 //   });
 
-api();
+// api();
